c33450b8e88828096b1b4f048b96dc0efb7d9cb8
xuang <1253217959@qq.com>
2019-02-23 18:30:26 +0800

    Initial commit project inint

---
+++ api/src/main/java/com/duoduo/service/impl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.duoduo.service.impl;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.duoduo.entity.TokenEntity;
+import com.duoduo.entity.UserEntity;
+import com.duoduo.service.TokenService;
+import com.duoduo.service.UserService;
+import io.renren.common.exception.RRException;
+import io.renren.common.validator.Assert;
+import com.duoduo.dao.UserDao;
+import com.duoduo.form.LoginForm;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service("userService")
+public class UserServiceImpl extends ServiceImpl<UserDao, UserEntity> implements UserService {
+	@Autowired
+	private TokenService tokenService;
+
+	@Override
+	public UserEntity queryByMobile(String mobile) {
+		return baseMapper.selectOne(new QueryWrapper<UserEntity>().eq("mobile", mobile));
+	}
+
+	@Override
+	public Map<String, Object> login(LoginForm form) {
+		UserEntity user = queryByMobile(form.getMobile());
+		Assert.isNull(user, "手机号或密码错误");
+
+		//密码错误
+		if(!user.getPassword().equals(DigestUtils.sha256Hex(form.getPassword()))){
+			throw new RRException("手机号或密码错误");
+		}
+
+		//获取登录token
+		TokenEntity tokenEntity = tokenService.createToken(user.getUserId());
+
+		Map<String, Object> map = new HashMap<>(2);
+		map.put("token", tokenEntity.getToken());
+		map.put("expire", tokenEntity.getExpireTime().getTime() - System.currentTimeMillis());
+
+		return map;
+	}
+
+}
